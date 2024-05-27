735f6e23f61f2c284d36e27d716e2df903e0a57f
ThisIsKhalid <iamkhalid.hs@gmail.com>
2024-05-27 17:54:54 +0600

    remove previous setup

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/MainRouter";
-
-function App() {
-  return <RouterProvider router={router}></RouterProvider>;
-}
-
-export default App;
