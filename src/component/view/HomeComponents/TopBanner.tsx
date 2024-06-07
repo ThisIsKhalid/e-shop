f2b90b50cb24fa91929013366fbc92695ac8b655
ThisIsKhalid <iamkhalid.hs@gmail.com>
2024-06-07 15:18:16 +0600

    top banner: added content

--- src/component/view/HomeComponents/TopBanner.tsx
+++ src/component/view/HomeComponents/TopBanner.tsx
@@ -1,6 +1,5 @@
-import Button from "../../ui/Button/Button";
-
 import { Swiper, SwiperSlide } from "swiper/react";
+import Button from "../../ui/Button/Button";
 
 // Import Swiper styles
 import "swiper/css";
@@ -20,10 +19,13 @@ export default function TopBanner() {
       <SwiperSlide className="h-full flex items-center md:flex-row flex-col-reverse">
         <div className="h-full w-full md:w-1/2 bg-gray-800 flex items-center justify-center p-5">
           <div>
-            <h1 className="text-white lg:text-6xl md:text-5xl text-3xl  font-bold">
+            <h1 className="text-white uppercase lg:text-6xl md:text-5xl text-3xl  font-bold">
               EID AL- ADHA 24
             </h1>
-            <Button variant="outline" className="mt-5 border-white text-white">
+            <Button
+              variant="outline"
+              className="mt-5 border-white text-white hover:bg-white hover:text-black"
+            >
               Shop Now
             </Button>
           </div>
@@ -39,17 +41,20 @@ export default function TopBanner() {
       <SwiperSlide className="h-full flex items-center md:flex-row flex-col-reverse">
         <div className="h-full w-full md:w-1/2 bg-gray-800 flex items-center justify-center p-5">
           <div>
-            <h1 className="text-white lg:text-6xl md:text-5xl text-3xl  font-bold">
-              EID AL- ADHA 24
+            <h1 className="text-white first-line:uppercase lg:text-6xl md:text-5xl text-3xl  font-bold">
+              Casual Shirt
             </h1>
-            <Button variant="outline" className="mt-5 border-white text-white">
+            <Button
+              variant="outline"
+              className="mt-5 border-white text-white hover:bg-white hover:text-black"
+            >
               Shop Now
             </Button>
           </div>
         </div>
         <div className="w-full md:w-1/2 h-full">
           <img
-            src="https://easyfashion.com.bd/wp-content/uploads/2024/05/Untitled-design-scaled.webp"
+            src="https://easyfashion.com.bd/wp-content/uploads/2024/05/Untitled-design-1-1536x887.webp"
             alt=""
             className="h-full w-full object-cover"
           />
@@ -58,36 +63,20 @@ export default function TopBanner() {
       <SwiperSlide className="h-full flex items-center md:flex-row flex-col-reverse">
         <div className="h-full w-full md:w-1/2 bg-gray-800 flex items-center justify-center p-5">
           <div>
-            <h1 className="text-white lg:text-6xl md:text-5xl text-3xl  font-bold">
-              EID AL- ADHA 24
+            <h1 className="text-white first-line:uppercase lg:text-6xl md:text-5xl text-3xl  font-bold">
+              Polo T-Shirt
             </h1>
-            <Button variant="outline" className="mt-5 border-white text-white">
+            <Button
+              variant="outline"
+              className="mt-5 border-white text-white hover:bg-white hover:text-black"
+            >
               Shop Now
             </Button>
           </div>
         </div>
         <div className="w-full md:w-1/2 h-full">
           <img
-            src="https://easyfashion.com.bd/wp-content/uploads/2024/05/Untitled-design-scaled.webp"
-            alt=""
-            className="h-full w-full object-cover"
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide className="h-full flex items-center md:flex-row flex-col-reverse">
-        <div className="h-full w-full md:w-1/2 bg-gray-800 flex items-center justify-center p-5">
-          <div>
-            <h1 className="text-white lg:text-6xl md:text-5xl text-3xl  font-bold">
-              EID AL- ADHA 24
-            </h1>
-            <Button variant="outline" className="mt-5 border-white text-white">
-              Shop Now
-            </Button>
-          </div>
-        </div>
-        <div className="w-full md:w-1/2 h-full">
-          <img
-            src="https://easyfashion.com.bd/wp-content/uploads/2024/05/Untitled-design-scaled.webp"
+            src="https://easyfashion.com.bd/wp-content/uploads/2024/05/Untitled-design-2-1536x1024.webp"
             alt=""
             className="h-full w-full object-cover"
           />
