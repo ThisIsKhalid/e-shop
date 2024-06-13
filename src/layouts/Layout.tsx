497225a093d089efb0fa41b0b5b781595d308655
ThisIsKhalid <iamkhalid.hs@gmail.com>
2024-06-13 22:26:57 +0600

    added footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../component/view/Header/Header";
+import Footer from "../component/view/Footer/Footer";
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
